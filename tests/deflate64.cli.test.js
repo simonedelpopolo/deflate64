cd5f9c1ac67a2e3d5ea8b8c60dbfddd7cf9d4d4f
simonedelpopolo <simonedelpopolo@outlook.com>
2021-12-12 13:54:25 +0000

    version 0.2.0 edited and corrected the test to spawn the project scope executable instead of the globally installed ;) importing the @simonedelpopolo/json-parse and use it in the test added test to get the stdout from the spawned process of deflate64.js

--- tests/deflate64.cli.test.js
+++ tests/deflate64.cli.test.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
 import { spawn } from 'child_process'
+import { parse } from '@simonedelpopolo/json-parse'
 
 test( 'testing deflate64 cli help command exit code equal to 0', async () => {
-    const deflate64 = spawn( 'deflate64', [
+    const deflate64 = spawn( './deflate64.js', [
         'help',
         'encode',
     ] )
@@ -13,3 +14,17 @@ test( 'testing deflate64 cli help command exit code equal to 0', async () => {
     } )
     
 } )
+
+test('testing deflate64 output of json string spawning a process and digest the output', async () => {
+    
+    const deflate64 = spawn('./deflate64.js', [
+        'encode',
+        '--string', 'hello',
+        '--json', 'true'
+    ])
+    
+    deflate64.stdout.on('data', async chunk => {
+        expect(await parse(chunk)).toStrictEqual({string:'eJzLSM3JyQcABiwCFQ=='})
+    })
+    
+})
