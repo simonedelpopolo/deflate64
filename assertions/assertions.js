8f440a30fd2e3de57a0035356e7837c47af63fe9
simonedelpopolo <simonedelpopolo@outlook.com>
2021-12-17 22:27:43 +0000

    version 1.3.10
    assertions.js improvements to the tests

--- assertions/assertions.js
+++ assertions/assertions.js
@@ -2,27 +2,52 @@ import { spawn } from 'child_process'
 import { parse } from '@simonedelpopolo/json-parse'
 import { deepEqual, ok } from 'assert/strict'
 
-const deflate64 = spawn('../deflate64.js', [
-    'encode',
-    '--string', 'hello',
-    '--json', 'true'
-])
+const assertion0 = () => {
+    
+    let expectedObject = { string: 'eJzLSM3JyQcABiwCFQ==' }
+    console.log( `\x1b[31m It checks if the returning variable is of type Object and if the exit code is`, 0, '\x1b[0m' )
+    
+    const deflate64 = spawn( '../executable.js', [
+        'encode',
+        '--string',
+        'hello',
+        '--json',
+        'true',
+    ] )
+    
+    deflate64.stdout.on( 'data', async chunk => {
+        deepEqual( await parse( chunk ), expectedObject )
+    } )
+    
+    deflate64.on( 'exit', exitCode => {
+        ok( exitCode === 0 )
+    } )
+}
 
-deflate64.stdout.on('data', async chunk => {
-    let obj = await parse(chunk)
-    deepEqual(obj, {string:'eJzLSM3JyQcABiwCFQ=='})
-    ok(typeof obj === 'object')
-})
+const assertion1 = () => {
+    
+    const expectedBuffer = Buffer.from( 'hello' )
+    console.log( `\x1b[31m It checks if the returning variable is of type Buffer is`, Buffer.isBuffer( expectedBuffer ), '\x1b[31mand if the exit code is', 0, '\x1b[0m' )
+    
+    const deflate64 = spawn( '../executable.js', [
+        'decode',
+        '--string',
+        '{"string":"eJzLSM3JyQcABiwCFQ=="}',
+        '--in-object',
+        'true',
+        '--spawn',
+        'true',
+    ] )
+    
+    deflate64.stdout.on( 'data', async chunk => {
+        deepEqual( chunk, expectedBuffer )
+    } )
+    
+    deflate64.on( 'exit', exitCode => {
+        ok( exitCode === 0 )
+    } )
+}
 
-const deflate642 = spawn('../deflate64.js', [
-    'decode',
-    '--string', '{"string":"eJzLSM3JyQcABiwCFQ=="}',
-    '--in-object', 'true',
-    '--spawn', 'true'
-])
+assertion0()
+assertion1()
 
-deflate642.stdout.on('data', async chunk => {
-    let string = chunk
-    deepEqual(string, Buffer.from('hello'))
-    ok(Buffer.isBuffer(string))
-})
