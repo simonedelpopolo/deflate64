c181ba4a8b1918192ae5e32040c287175dd412ad
simonedelpopolo <simonedelpopolo@outlook.com>
2021-12-28 15:19:06 +0000

    version 1.3.13
    update the way to call assertions.js
    it can be done by specify, in the shell, the number of the assertion to run
    example:
    
    cd assertion && node assertions.js assertion0
    
    added the scripts into the package.json to run the available assertions.
    
    some adjustment to the code of deflate64 are in the making.

--- assertions/assertions.js
+++ assertions/assertions.js
@@ -1,53 +1,74 @@
+console.time( 'assertions finished ' )
+import { EventEmitter } from 'events'
 import { spawn } from 'child_process'
 import { parse } from '@simonedelpopolo/json-parse'
 import { deepEqual, ok } from 'assert/strict'
 
-const assertion0 = () => {
+const AssertionEvent = new EventEmitter()
+
+AssertionEvent.on( 'end', () => {
+    console.timeEnd( 'assertions finished ' )
+} )
+
+const Assertions = {
     
-    let expectedObject = { string: 'eJzLSM3JyQcABiwCFQ==' }
-    console.log( `\x1b[31m It checks if the returning variable is of type Object and if the exit code is`, 0, '\x1b[0m' )
+    assertion0 : async () => {
+        let expectedObject = { string: 'eJzLSM3JyQcABiwCFQ==' }
+        console.log( `\x1b[31m It checks if the returning variable is of type Object and if the exit code is`, 0, '\x1b[0m' )
     
-    const deflate64 = spawn( '../executable.js', [
-        'encode',
-        '--string',
-        'hello',
-        '--json',
-        'true',
-    ] )
+        const deflate64 = spawn( '../executable.js', [
+            'encode',
+            '--string',
+            'hello',
+            '--json',
+            'true',
+        ] )
     
-    deflate64.stdout.on( 'data', async chunk => {
-        deepEqual( await parse( chunk ), expectedObject )
-    } )
+        deflate64.stdout.on( 'data', async chunk => {
+            deepEqual( await parse( chunk ), expectedObject )
+        } )
     
-    deflate64.on( 'exit', exitCode => {
-        ok( exitCode === 0 )
-    } )
+        deflate64.on( 'exit', exitCode => {
+            ok( exitCode === 0 )
+        } )
+    },
+    
+    assertion1 : async () => {
+        const expectedBuffer = Buffer.from( 'hello' )
+        console.log( `\x1b[31m It checks if the returning variable is of type Buffer is`, Buffer.isBuffer( expectedBuffer ), '\x1b[31mand if the exit code is', 0, '\x1b[0m' )
+    
+        const deflate64 = spawn( '../executable.js', [
+            'decode',
+            '--string',
+            '{"string":"eJzLSM3JyQcABiwCFQ=="}',
+            '--in-object',
+            'true',
+            '--spawn',
+            'true',
+        ] )
+    
+        deflate64.stdout.on( 'data', async chunk => {
+            deepEqual( chunk, expectedBuffer )
+        } )
+    
+        deflate64.on( 'exit', exitCode => {
+            ok( exitCode === 0 )
+        } )
+    },
+    
 }
 
-const assertion1 = () => {
+process.argv.splice( 0, 2 )
+
+if(  process.argv.length > 0 ){
     
-    const expectedBuffer = Buffer.from( 'hello' )
-    console.log( `\x1b[31m It checks if the returning variable is of type Buffer is`, Buffer.isBuffer( expectedBuffer ), '\x1b[31mand if the exit code is', 0, '\x1b[0m' )
+    await Assertions[ process.argv ]()
+    AssertionEvent.emit( 'end' )
     
-    const deflate64 = spawn( '../executable.js', [
-        'decode',
-        '--string',
-        '{"string":"eJzLSM3JyQcABiwCFQ=="}',
-        '--in-object',
-        'true',
-        '--spawn',
-        'true',
-    ] )
+}else {
     
-    deflate64.stdout.on( 'data', async chunk => {
-        deepEqual( chunk, expectedBuffer )
-    } )
+    for( const assertion in Assertions )
+        await Assertions[ assertion ]()
     
-    deflate64.on( 'exit', exitCode => {
-        ok( exitCode === 0 )
-    } )
+    AssertionEvent.emit( 'end' )
 }
-
-assertion0()
-assertion1()
-
