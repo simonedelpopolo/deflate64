07e8dc3557f153521e69e0ec13f94b29d6178ab0
simonedelpopolo <simonedelpopolo@outlook.com>
2021-12-29 09:17:12 +0000

    v2.0.0 add comment

--- executable.js
+++ executable.js
@@ -4,4 +4,8 @@ import deflate64 from './index.js'
 // It gets the command line arguments splicing out from `process.argv` the paths for node and executable.js
 process.argv.splice( 0, 2 )
 
+/**
+ * Entry point to deflate64.
+ */
 deflate64( process.argv )
+
