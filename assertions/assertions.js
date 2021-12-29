244c154a3ef3d3a2fd5b2d09149a1545333e58f9
simonedelpopolo <simonedelpopolo@outlook.com>
2021-12-29 12:25:20 +0000

    draft for assertions test

--- assertions/assertions.js
+++ assertions/assertions.js
@@ -1,8 +1,8 @@
 console.time( 'assertions finished ' )
 import { EventEmitter } from 'events'
-import { spawn } from 'child_process'
 import { parse } from '@simonedelpopolo/json-parse'
 import { deepEqual, ok } from 'assert/strict'
+import deflate64 from '../index.js'
 
 const AssertionEvent = new EventEmitter()
 
@@ -15,47 +15,9 @@ const Assertions = {
     assertion0 : async () => {
         let expectedObject = { string: 'eJzLSM3JyQcABiwCFQ==' }
         console.log( `\x1b[31m It checks if the returning variable is of type Object and if the exit code is`, 0, '\x1b[0m' )
-    
-        const deflate64 = spawn( '../executable.js', [
-            'encode',
-            '--string',
-            'hello',
-            '--json',
-            'true',
-        ] )
-    
-        deflate64.stdout.on( 'data', async chunk => {
-            deepEqual( await parse( chunk ), expectedObject )
-        } )
-    
-        deflate64.on( 'exit', exitCode => {
-            ok( exitCode === 0 )
-        } )
+        
+        deflate64(['decode', '--string=eJzLSM3JyQcABiwCFQ==', '--json=true'])
     },
-    
-    assertion1 : async () => {
-        const expectedBuffer = Buffer.from( 'hello' )
-        console.log( `\x1b[31m It checks if the returning variable is of type Buffer is`, Buffer.isBuffer( expectedBuffer ), '\x1b[31mand if the exit code is', 0, '\x1b[0m' )
-    
-        const deflate64 = spawn( '../executable.js', [
-            'decode',
-            '--string',
-            '{"string":"eJzLSM3JyQcABiwCFQ=="}',
-            '--in-object',
-            'true',
-            '--spawn',
-            'true',
-        ] )
-    
-        deflate64.stdout.on( 'data', async chunk => {
-            deepEqual( chunk, expectedBuffer )
-        } )
-    
-        deflate64.on( 'exit', exitCode => {
-            ok( exitCode === 0 )
-        } )
-    },
-    
 }
 
 process.argv.splice( 0, 2 )
