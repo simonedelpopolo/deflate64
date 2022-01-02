681f1380afa259ac10b72773bc4eaa432e7c308c
simonedelpopolo <simonedelpopolo@outlook.com>
2022-01-02 09:44:07 +0000

    v2.0.0 added exported function d64

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import { entryPoint, entryPointSymbol  } from './lib/d64/exporter.js'
 
 /**
- * Deflate 64 entry point.
+ * The deflate64 entry point and default exported function.
  *
  * @public
  * @param {string[]} argv - Process.argv.splice( 0, 2 ) command line arguments splicing out from `process.argv` the paths for node and executable.js.
  * @returns {Promise | PromiseFulfilledResult<any> | PromiseRejectedResult<any>}
  */
 export default function deflate64( argv ){
+    
+    return  entryPoint[ entryPointSymbol ]( argv )
+}
+
+/**
+ * The d64 entry point and exported function.
+ *
+ * @public
+ * @param {string[]} argv - Process.argv.splice( 0, 2 ) command line arguments splicing out from `process.argv` the paths for node and executable.js.
+ * @returns {Promise | PromiseFulfilledResult<any> | PromiseRejectedResult<any>}
+ */
+export function d64( argv ){
     
     return  entryPoint[ entryPointSymbol ]( argv )
 }
