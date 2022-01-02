14dfebf78091dac73b8528f72662ebca5e92ba4e
simonedelpopolo <simonedelpopolo@outlook.com>
2022-01-02 09:45:56 +0000

    v2.0.0 now using function d64 instead of deflate64

--- executable.js
+++ executable.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import deflate64 from './index.js'
+import { d64 } from './index.js'
 
 // It gets the command line arguments splicing out from `process.argv` the paths for node and executable.js
 process.argv.splice( 0, 2 )
@@ -7,6 +7,6 @@ process.argv.splice( 0, 2 )
 /**
  * Entry point to deflate64.
  */
-await deflate64( process.argv )
+await d64( process.argv )
 
 process.exit( 0 )
