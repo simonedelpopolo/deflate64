cec302d6ad6a77a2bc3e44c6cea54d307d656c07
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-01-15 13:51:36 +0000

    v2.1.10-beta ⬇︎
    added Components to d64 dedicate website
    
    pulled the Update issue templates and code of conducts

---
+++ website/src/components/Documentation.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Documentation extends Component{
+    
+    constructor( props ) {
+        super( props )
+        
+    }
+    
+    render() {
+        return(
+            <React.StrictMode>
+                <h1> Documentation! </h1>
+                <div className={'container'}>
+                    <section />
+                </div>
+                
+            </React.StrictMode>
+        
+        )
+    }
+}
+
+export default Documentation
---
+++ website/src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Footer extends Component{
+    
+    constructor( properties ) {
+        super( properties )
+    }
+    
+    render() {
+        
+        return (
+            <footer>
+                &copy;d64!
+            </footer>
+        )
+    }
+}
+
+export default Footer
