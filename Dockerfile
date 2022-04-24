7b0d13a2040f1eafabfb977c0f938dcf272a2fd8
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-24 10:31:35 +0000

    v3.0.0 work in progress

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:16.14.2-alpine
-
-WORKDIR ~
-
-RUN npm i -g deflate64
-
-ENTRYPOINT ["/bin/bash"]
--- d64.js
+++ d64.js
@@ -1,12 +1,39 @@
-#!/usr/bin/env node
-import { d64 } from './index.js'
+#!/usr/bin/env node --experimental-json-modules
+import { d64_process } from './lib/input/d64/d64_process.js'
+import { decode } from './lib/d64/decode.js'
+import { encode } from './lib/d64/encode.js'
+import { entry_point } from '@cli-blaze/input'
 
 // It gets the command line arguments splicing out from `process.argv` the paths for node and d64.js
 process.argv.splice( 0, 2 )
 
+process.title = 'd64'
+
 /**
  * Entry point to deflate64.
  */
-await d64( process.argv )
+const d64 = await entry_point( process.argv, { d64: d64_process, executable:[ 'd64' ] } )
 
-process.exit( 0 )
+switch ( Object.keys( d64[ 'command' ] )[ 0 ] ) {
+
+    case 'decode':
+
+        await decode( d64.flag )
+
+        break
+
+    case 'encode':
+
+        await encode( d64.flag )
+
+        break
+
+    case 'help':
+
+        break
+
+    default:
+
+        break
+
+}
